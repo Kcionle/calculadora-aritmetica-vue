06705476540d7b13c8f3cc41255f4d95653caac1
Cassio Pinheiro <cassiochan.u@gmail.com>
2024-02-02 08:09:49 -0300

    criando as primerias atualizaçoes

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,79 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+export default {
+  data() {
+    return {
+      numero1: 0,
+      numero2: 0,
+      operacao: "+",
+      resultado: 0,
+    };
+
+//O método data() é utilizado para definir os dados que serão utilizados no componente. Ele retorna um objeto contendo as propriedades numero1, numero2, operacao, e resultado, que são reativos. Isso significa que qualquer alteração nesses dados irá automaticamente refletir na interface do usuário.
+  },
+  methods: {
+    calcular() {
+      switch (this.operacao) {
+        case "+":
+          this.resultado = this.numero1 + this.numero2;
+          break;
+        case "-":
+          this.resultado = this.numero1 - this.numero2;
+          break;
+        case "*":
+          this.resultado = this.numero1 * this.numero2;
+          break;
+        case "/":
+          this.resultado = this.numero1 / this.numero2;
+          break;
+        default:
+          this.resultado = 0;
+
+// Este método é chamado sempre que há uma mudança nos valores de numero1, numero2 ou operacao. Ele realiza o cálculo com base nos valores atuais dessas variáveis e atribui o resultado à propriedade resultado.
+//A estrutura de controle switch é utilizada para determinar qual operação aritmética deve ser realizada com base no valor de operacao.
+      }
+    },
+  },
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div id="app" class="calculator-container mt-5" style="max-width: 500px;">
+    <h1 class="mb-4">Calculadora Aritmética</h1>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div class="form-group">
+      <label for="numero1">Número 1:</label>
+      <input type="number" class="form-control" v-model="numero1" @input="calcular" />
     </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="form-group">
+      <label for="numero2">Número 2:</label>
+      <input type="number" class="form-control" v-model="numero2" @input="calcular" />
+    </div>
+
+    <div class="form-group">
+      <label for="operacao">Operação:</label>
+      <select class="form-control" v-model="operacao" @change="calcular">
+        <option value="+">Soma (+)</option>
+        <option value="-">Subtração (-)</option>
+        <option value="*">Multiplicação (*)</option>
+        <option value="/">Divisão (/)</option>
+      </select>
+    </div>
+
+    <div class="form-group">
+      <label for="resultado">Resultado:</label>
+      <input type="text" class="form-control" v-model="resultado" readonly />
+    </div>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<style>
+.calculator-container {
+  background-color: #5e37ca;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  color: rgb(243, 248, 246);
 }
 </style>
+
